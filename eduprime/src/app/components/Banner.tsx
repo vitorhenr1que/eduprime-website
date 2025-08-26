f63e5c70d6104f1a11d41a0a9b72362443da5b23
Vitor Henrique <vhpsantos@gmail.com>
2025-08-26 12:52:58 -0300

    delete eduprime

--- eduprime/src/app/components/Banner.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import banner from '../../../public/Banner.png'
-export default function Banner() {
-return (
-<section className="banner container sr-banner">
-<Image src={banner} width={2000} alt="Banner principal EduPrime" loading="eager" quality={100}/>
-</section>
-);
-}
